c0209b09248de7131c57711b0b4d7057a3c75864
ana975 <59800380+ana975@users.noreply.github.com>
2021-09-13 21:02:39 +0200

    Add files via upload

--- ProjectIntelliJ/src/main/scala/org/example/Pingpong.scala
+++ ProjectIntelliJ/src/main/scala/org/example/Pingpong.scala
@@ -2,8 +2,8 @@ package org.example
 
 import akka.actor.{Actor, ActorSystem, Props}
 
-object Pingpong extends App {
 
+object Pingpong extends App {
   class Actor1 extends Actor {
     def receive = {
       case "Ping" =>
@@ -13,7 +13,7 @@ object Pingpong extends App {
 
   val system = ActorSystem("System1")
   val actor = system.actorOf(Props[Actor1], "Actor1")
-
-  system.terminate()
-
 }
+
+
+
