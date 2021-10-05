6dbf4c20ee695cf23dc6bcbaded4ced117a081ac
mafernandez-stratio <mafernandez@stratio.com>
2021-10-05 11:14:14 +0200

    Akka cluster Client

--- ProjectIntelliJ/src/main/scala/org/example/DemoClient.scala
+++ ProjectIntelliJ/src/main/scala/org/example/DemoClient.scala
@@ -1,48 +1,81 @@
 package org.example
 
-import akka.actor.{Actor, ActorLogging, ActorPath, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorPath, ActorRef, ActorSystem, Props}
 import akka.cluster.client.{ClusterClient, ClusterClientSettings}
+import akka.pattern.ask
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import scala.io.StdIn
 
 object DemoClient {
 
+  case class Query(msg: String)
+
   def main(args : Array[String]) {
     val config = ConfigFactory.parseString("""
-     akka {
-       log-dead-letters = OFF
-       actor {
-         provider = "akka.remote.RemoteActorRefProvider"
-       }
+        akka {
+          loglevel = "warning"
+          actor {
+            provider = "akka.cluster.ClusterActorRefProvider"
+          }
+          remote {
+            transport = "akka.remote.netty.NettyRemoteTransport"
+            netty.tcp {
+              hostname = "127.0.0.1"
+              port = 3000
+            }
+          }
+          extensions = ["akka.cluster.client.ClusterClientReceptionist"]
+          cluster.client {
+            initial-contacts = ["akka.tcp://NostromoCluster@127.0.0.1:2551/system/receptionist"]
+            establishing-get-contacts-interval = 3s
+            refresh-contacts-interval = 60s
+            heartbeat-interval = 2s
+            acceptable-heartbeat-pause = 13s
+            buffer-size = 1000
+            reconnect-timeout = off
+          }
+        }
+        """)
 
-       remote {
-         transport = "akka.remote.netty.NettyRemoteTransport"
-         log-remote-lifecycle-events = off
-         netty.tcp {
-          hostname = "localhost"
-          port = 5000
-         }
-       }
-     }""")
+    val system = ActorSystem("ClientSystem", config)
 
-    val system = ActorSystem("OUTSIDER-SYSTEM", ConfigFactory.load(config))
+    implicit val timeout = Timeout(60 seconds)
+
+    // create the client
     val initialContacts = Set(
-      ActorPath.fromString("akka.tcp://ClusterSystem@localhost:2551/system/receptionist"))
-    val cc = system.actorOf(ClusterClient.props(
-      ClusterClientSettings(system).withInitialContacts(initialContacts)), "os-client")
-    val ccActor = system.actorOf(Props[ClusterClientActor], "ccActor")
+      ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/system/receptionist"))
+    val settings = ClusterClientSettings(system).withInitialContacts(initialContacts)
+    val client = system.actorOf(ClusterClient.props(settings), "client")
+    val clientActor = system.actorOf(Props(new ClientActor(client)), "clientActor")
+    Await.result(clientActor ? "ping", timeout.duration)
 
-    cc ! ClusterClient.Send("/user/master", ccActor, localAffinity = true)
-    var line = ""
-    while ({line = StdIn.readLine (); line != null}) {println (line)}
-     cc ! ClusterClient.Send("/user/master", line, localAffinity = true)
+    var message = ""
+    while (message.toLowerCase() != "exit") {
+      message = StdIn.readLine("Query: ")
+      Await.result(clientActor ? Query(message), timeout.duration)
+    }
+
+    system.terminate()
+
   }
 
-  class ClusterClientActor extends Actor with ActorLogging {
+  class ClientActor(clientActor: ActorRef) extends Actor with ActorLogging {
+    implicit val timeout = Timeout(60 seconds)
+    var demoClient: ActorRef = null
     def receive = {
-      case e =>
-        log.info(s"from cluster-client : $e : $sender")
-        println (e)
+      case "ping" =>
+        demoClient = sender()
+        clientActor ! ClusterClient.Send("/user/master", "ping", localAffinity = false)
+      case Query(message) =>
+        demoClient = sender()
+        clientActor ! ClusterClient.Send("/user/master", message, localAffinity = false)
+      case msg: String =>
+        println(s"Response: $msg")
+        demoClient ! ""
     }
   }
 }
